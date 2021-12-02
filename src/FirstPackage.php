559c883b2e63c282c7c05fe491f5745dcacb37d5
Salem Saber <salem.saber97@gmail.com>
2021-12-02 08:12:28 +0200

    change name

--- src/FirstPackage.php
+++ src/FirstPackage.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Salem\FirstPackage;
+namespace SalemSaber\FirstPackage;
 
 class FirstPackage
 {
--- src/FirstPackageServiceProvider.php
+++ src/FirstPackageServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Salem\FirstPackage;
+namespace SalemSaber\FirstPackage;
 
 use Illuminate\Support\ServiceProvider;
 
